e537991557f8d152979ec246a2d7cccd8714cf22
sinemdonmez <sinemdonmez1@hotmail.com.tr>
2023-12-06 13:23:54 +0100

    3 and 4 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let myarray =string.split("");
+    result= myarray.reverse();
+    return result.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...extras) {
+    for(const element of extras){
+        if(array.indexOf(element)!=-1){
+            array.splice(array.indexOf(element),1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
