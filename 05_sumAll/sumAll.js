5c19a85deff05616d3bc9c3678d5703f7d910cbb
sinemdonmez <sinemdonmez1@hotmail.com.tr>
2023-12-06 13:32:14 +0100

    05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(from,to) {
+    if(typeof(from)!=typeof(2) || typeof(to)!=typeof(2) || from<0 || to<0)
+        return 'ERROR'
+    let sum = 0;
+    if(from>to){
+        let temp = to;
+        to = from;
+        from = temp;
+    }
+    for(i=0; i<to; i++){
+        sum += from+i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
