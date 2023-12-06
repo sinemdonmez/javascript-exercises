32a8fb1e2cc4915cf099e2431fd34e0285cb1e7c
sinemdonmez <sinemdonmez1@hotmail.com.tr>
2023-12-06 13:49:09 +0100

    07 done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  //C = 5/9 x (F-32)
+  fahrenheit = (5/9*(fahrenheit-32)).toFixed(1)
+  if(fahrenheit[fahrenheit.length-1]=="0"){
+    fahrenheit = fahrenheit.substring(0, fahrenheit.length-2);
+    
+  }
+  return Number(fahrenheit);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  //°F = °C * 9/5 + 32
+  fahrenheit = (celcius *9/5+32).toFixed(1)
+  if(fahrenheit[fahrenheit.length-1]=="0"){
+    fahrenheit = fahrenheit.substring(0, fahrenheit.length-2);
+  }
+  return Number(fahrenheit);
 };
 
 // Do not edit below this line
