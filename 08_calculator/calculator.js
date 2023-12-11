12a468022aecf0df992e2ae0fa0211cb3a891f0c
sinemdonmez <sinemdonmez1@hotmail.com.tr>
2023-12-11 22:52:25 +0100

    08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, element) => total + element, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, element)=> total*element, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a==0) return 1;
+  let total=1;
+  for(let i=2; i<a+1; i++){
+    total=total*i;
+  }
+  return total;
 };
 
 // Do not edit below this line
