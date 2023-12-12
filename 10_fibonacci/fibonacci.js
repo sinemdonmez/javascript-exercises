f05bab88f99af00eca4589fa14f1cb3b98d43b63
sinemdonmez <sinemdonmez1@hotmail.com.tr>
2023-12-12 15:01:58 +0100

    10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num<0){
+        return "OOPS";
+    }
+    let start = 1;
+    let last = 1;
+    for(let i=2; i<num; i++){
+        let temp = start + last;
+        start = last;
+        last = temp;
+    }
+    return last;
 };
 
 // Do not edit below this line
