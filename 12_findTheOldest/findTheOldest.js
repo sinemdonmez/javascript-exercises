29d964169fa06e4461b16043ec4c580790b063aa
sinemdonmez <sinemdonmez1@hotmail.com.tr>
2023-12-12 15:14:56 +0100

    12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort(function(a,b){
+        if(a.yearOfDeath==null){
+            a.yearOfDeath=2023;
+        }
+        if(b.yearOfDeath==null){
+            b.yearOfDeath=2023;
+        }
+        const lastInventor = a.yearOfDeath - a.yearOfBirth;
+        const nextInventor = b.yearOfDeath - b.yearOfBirth;
+        return lastInventor > nextInventor ? -1 : 1;
+    });
 
+    return oldest[0];
 };
 
 // Do not edit below this line
