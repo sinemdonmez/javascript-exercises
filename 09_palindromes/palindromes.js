d03bb9e10a332cd840642e11b730047bb033614f
sinemdonmez <sinemdonmez1@hotmail.com.tr>
2023-12-12 14:58:02 +0100

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (check) {
+    let str = check.toLowerCase();
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    str = str.replace(/\s/g,"");
+
+    for(let i=0; i<str.length; i+=2){
+        console.log(str[i])
+        console.log(str[str.length-1-i])
+        if(str[i]!=str[str.length-1-i])
+            return false;
+    }
+
+    return true;
+        
 
 };
 
